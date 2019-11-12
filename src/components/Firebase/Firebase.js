5224c5711d06e09418624c4480ad76c8adec5f6b
Rajat Verma <rajatverma5885045@gmail.com>
2019-11-12 12:13:29 +0530

    added Config file

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -1,16 +1,7 @@
 import app from "firebase/app"; //import firebase api
 import "firebase/auth"; //responsible for authentication
 
-var config = {
-  apiKey: "AIzaSyDKLWI2GCULk7cwthUfO6Kq7PEVAUNLoao",
-  authDomain: "react-firebase-884dd.firebaseapp.com",
-  databaseURL: "https://react-firebase-884dd.firebaseio.com",
-  projectId: "react-firebase-884dd",
-  storageBucket: "react-firebase-884dd.appspot.com",
-  messagingSenderId: "192664896619",
-  appId: "1:192664896619:web:24deec1725e8e192fc9d2e",
-  measurementId: "G-QVFW3G0784"
-};
+import { config } from './Config';
 
 class Firebase {
   constructor() {
