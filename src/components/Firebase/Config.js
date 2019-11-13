e4dd27cbf4c32e8823977ada220cdb46b4a702dc
Rajat Verma <rajatverma5885045@gmail.com>
2019-11-13 22:41:46 +0530

    removed config.js from .gitignore

---
+++ src/components/Firebase/Config.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyDKLWI2GCULk7cwthUfO6Kq7PEVAUNLoao",
+    authDomain: "react-firebase-884dd.firebaseapp.com",
+    databaseURL: "https://react-firebase-884dd.firebaseio.com",
+    projectId: "react-firebase-884dd",
+    storageBucket: "react-firebase-884dd.appspot.com",
+    messagingSenderId: "192664896619",
+    appId: "1:192664896619:web:24deec1725e8e192fc9d2e",
+    measurementId: "G-QVFW3G0784"
+  };
+
+export { config };
